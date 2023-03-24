76c47e15c7c9b58f9a80de40f563d07b1d6d2d42
MYAkushev <m.yakushev.99@gmail.com>
2023-03-24 21:02:41 +0500

    Commit Program_7

---
+++ src/ru/ibs/program7/Main.java
@@ -0,0 +1,29 @@
+package ru.ibs.program7;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        final double x = 1.5;
+        final double y = 5;
+        final double z = -327.34;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Введите размер массива (размер массива должен быть > 0):");
+        int length = input.nextInt();
+        double[] array = new double[length];
+
+        System.out.println("Введите значения массива");
+        for (int i=0; i<array.length; i++) {
+            System.out.println("array["+i+"]=");
+            array[i] = input.nextDouble();
+            if (array[i] == x || array[i] == y || array[i] == z) {
+                System.out.println("Данное значение имеется в константах");
+            }
+        }
+    }
+}
+
